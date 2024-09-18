51e5745f9c3bf63d1c99b244eb53db4d43e66cf6
ALIF-AL-RAZI <alifalrazi1@gmail.com>
2024-09-18 21:10:21 +0600

    error handling

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const About = () => {
+
+    // throw new Error("There is something wrong!!")
   return (
     <div className='mt-10'>Pariatur excepteur sit ut mollit.</div>
   )
